cc066d9e127532a6bb9a2e3a97d1d4dc099b559f
Miguel Cemiller <miguelcemiller@gmail.com>
2023-04-18 16:41:28 +0800

    fdsf

--- client_window.py
+++ client_window.py
@@ -142,8 +142,7 @@ class ClientWindow(CustomWindow):
 
     def stop_client(self):
         if hasattr(self, 'connection_thread') and self.connection_thread.isRunning():
-            if self.connection_thread.connected:
-                self.connection_thread.send_message('Client disconnected')
+            self.connection_thread.send_message('Client disconnected')
             self.connection_thread.terminate()
             self.connection_thread.wait()
             del self.connection_thread
