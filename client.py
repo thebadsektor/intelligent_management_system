0092b35e6e6a984c1d6f2f345998765d49140d53
Miguel Cemiller <miguelcemiller@gmail.com>
2023-04-18 18:25:24 +0800

    set idle time threshold to 10 seconds

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import time
 from monitor import *
 
 # Set constants
-IDLE_TIME_THRESHOLD = 5 # 5 minutes
+IDLE_TIME_THRESHOLD = 5 # 5 seconds
 CPU_USAGE_IDLE_THRESHOLD = 10 # 10%
 
 def connect_to_server(s, callback=None):
